eacb86983691415889e9ee4254eef4d8cfbd56b0
Khozema Nullwala <khozema.nullwala@gmail.com>
2016-10-04 05:23:29 +0530

    Updated README.md and gitignore files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Collaboration Portal
+
+This project provides the base to start the collaboration website using angular js as the front-end technology that uses JSON format for recieving and sending data to the back end application designed using spring framework.
+
--- collaboration/WebContent/app/app.routes.js
+++ collaboration/WebContent/app/app.routes.js
@@ -1,27 +1,27 @@
 app.config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider) {	
 	$routeProvider.
-		when('/home/', {
+		when('/home', {
 			templateUrl: 'app/components/page/homeView.html',
 			controller: 'HomeController',
 			controllerAs: 'homeCtrl'
 		})
-		.when('/about/', {
+		.when('/about', {
 			templateUrl: 'app/components/page/aboutView.html',
 			controller: 'AboutController',
 			controllerAs: 'aboutCtrl'
 		})
-		.when('/contact/', {
+		.when('/contact', {
 			templateUrl: 'app/components/page/contactView.html',
 			controller: 'ContactController',
 			controllerAs: 'contactCtrl'
 		})
-		.when('/blog/', {
+		.when('/blog', {
 			templateUrl: 'app/components/blog/blogView.html',
 			controller: 'BlogController',
 			controllerAs: 'blogCtrl'
 		})
 		.otherwise({
-			redirectTo: '/home/'
+			redirectTo: '/home'
 		})
-	$locationProvider.html5Mode(true); // requires a base tag	
+	//$locationProvider.html5Mode(true); // requires a base tag	
 }]);
